6ae2f511d4243e8632e3c4d33d8304a9234c3b9f
Ruba Mohmand <ruba.mohmand17@gmail.com>
2025-01-04 19:50:05 +0500

    Update host and port configuration

--- app.py
+++ app.py
@@ -2,7 +2,10 @@ from flask import Flask, render_template, request, jsonify
 import openai
 import os
 from dotenv import load_dotenv
+import os
 
+port = int(os.environ.get("PORT", 5000))
+app.run(host='0.0.0.0', port=port)
 load_dotenv()
 
 app = Flask(__name__)
